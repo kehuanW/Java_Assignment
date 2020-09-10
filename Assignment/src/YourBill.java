6c902d9e7f7af64c98ed99a52500ffe2f2480ce3
Kehuan <kehuanwang123@gmail.com>
2020-09-10 22:03:40 +0800

    减少新的Scanner类； 修改变量名称

--- Assignment/src/YourBill.java
+++ Assignment/src/YourBill.java
@@ -3,63 +3,56 @@ import java.util.Scanner;
 public class YourBill {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         //Scanner the name, quantity and price of item 1
         System.out.println("Input name of item 1:");
-        Scanner item1_name = new Scanner(System.in);
-        String item1_name_string = item1_name.nextLine();
+        String item1Name = scanner.nextLine();
 
         System.out.println("Input quantity of item 1:");
-        Scanner item1_quantity = new Scanner(System.in);
-        long item1_quantity_long = item1_quantity.nextLong();
+        long item1Quantity = scanner.nextLong();
 
         System.out.println("Input price of item 1:");
-        Scanner item1_price = new Scanner(System.in);
-        double item1_price_double = item1_price.nextDouble();
+        double item1Price = scanner.nextDouble();
 
 
         //Scanner the name, quantity and price of item 2
         System.out.println("Input name of item 2:");
-        Scanner item2_name = new Scanner(System.in);
-        String item2_name_string = item2_name.nextLine();
+        String item2Name = scanner.nextLine();
 
         System.out.println("Input quantity of item 2:");
-        Scanner item2_quantity = new Scanner(System.in);
-        long item2_quantity_long = item2_quantity.nextLong();
+        long item2Quantity = scanner.nextLong();
 
         System.out.println("Input price of item 2:");
-        Scanner item2_price = new Scanner(System.in);
-        double item2_price_double = item2_price.nextDouble();
+        double item2Price = scanner.nextDouble();
 
 
         //Scanner the name, quantity and price of item 3
         System.out.println("Input name of item 3:");
-        Scanner item3_name = new Scanner(System.in);
-        String item3_name_string = item3_name.nextLine();
+        String item3Name = scanner.nextLine();
 
         System.out.println("Input quantity of item 3:");
-        Scanner item3_quantity = new Scanner(System.in);
-        long item3_quantity_long = item3_quantity.nextLong();
+        long item3Quantity = scanner.nextLong();
 
         System.out.println("Input price of item 3:");
-        Scanner item3_price = new Scanner(System.in);
-        double item3_price_double = item3_price.nextDouble();
+        double item3Price = scanner.nextDouble();
 
 
         //calculate each item total price
-        double item1_total = item1_price_double * item1_quantity_long;
-        double item2_total = item2_price_double * item2_quantity_long;
-        double item3_total = item3_price_double * item3_quantity_long;
+        double item1Total = item1Price * item1Quantity;
+        double item2Total = item2Price * item2Quantity;
+        double item3Total = item3Price * item3Quantity;
 
 
         //calculate the total price of all items
-        double subtotal = item1_total + item2_total + item3_total;
+        double subtotal = item1Total + item2Total + item3Total;
 
         //calculate the actual tax the user should pay
-        double items_tax = subtotal * 0.0625;
+        double itemsTax = subtotal * 0.0625;
 
 
         //calculate the final amount
-        double Total = subtotal + items_tax;
+        double Total = subtotal + itemsTax;
 
         //crate lengthFormat
         LengthFormat lengthFormat = new LengthFormat();
@@ -79,24 +72,24 @@ public class YourBill {
         System.out.println();
 
         //2nd line: information of item 1
-        System.out.printf("%-30s",lengthFormat.Lengthcontrol(item1_name_string,30));
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item1_quantity_long,10));
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item1_price_double,10));
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item1_total,10));
+        System.out.printf("%-30s",lengthFormat.Lengthcontrol(item1Name,30));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item1Quantity,10));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item1Price,10));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item1Total,10));
         System.out.println();
 
         //3th line: information of item 2
-        System.out.printf("%-30s",lengthFormat.Lengthcontrol(item2_name_string,30));
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item2_quantity_long,10));
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item2_price_double,10));
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item2_total,10));
+        System.out.printf("%-30s",lengthFormat.Lengthcontrol(item2Name,30));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item2Quantity,10));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item2Price,10));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item2Total,10));
         System.out.println();
 
         //4th line: information of item 3
-        System.out.printf("%-30s",lengthFormat.Lengthcontrol(item3_name_string,30));
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item3_quantity_long,10));
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item3_price_double,10));
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item3_total,10));
+        System.out.printf("%-30s",lengthFormat.Lengthcontrol(item3Name,30));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item3Quantity,10));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item3Price,10));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(item3Total,10));
         System.out.println();
 
         //5th line: subtotal
@@ -106,7 +99,7 @@ public class YourBill {
 
         //6th line: tax
         System.out.printf("%-50s","6.25% sales tax");
-        System.out.printf("%-10s",lengthFormat.Lengthcontrol(items_tax,10));
+        System.out.printf("%-10s",lengthFormat.Lengthcontrol(itemsTax,10));
         System.out.println();
 
         //7th line: total
@@ -119,7 +112,6 @@ public class YourBill {
 
 class LengthFormat {
     /**
-     *
      * @param object Variables that need to be formatted
      * @param characterNumber Maximum number of characters in the variable
      * @return A string that meets the length and decimal places requirements
